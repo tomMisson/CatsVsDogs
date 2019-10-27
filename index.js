0f5d05e53e0b5d266996e87c417ea6192e0fa7f6
Thomas Misson <11tmisson@gmail.com>
2019-10-27 10:58:37 +0000

    protoype build"

---
+++ index.js
@@ -0,0 +1,55 @@
+
+const express = require('express')
+const app = express()
+const port = 3000;
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const bodyParser = require('body-parser');
+
+var dogCount = 0;
+var catCount = 0;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/contactall', function(req, res){
+    client.messages.list({to: '+447481337208'})
+               .then(messages => messages.forEach(m => console.log(m.from)));
+})
+
+app.post('/sms', (req, res) => {
+    const twiml = new MessagingResponse();
+    
+    console.log(req.body.Body);
+
+    if(req.body.Body.toUpperCase() == "DOG" || req.body.Body.toUpperCase() == "CAT")
+    {
+        if(req.body.Body.toUpperCase()==("DOG"))
+        {
+            dogCount ++;
+        }
+        if(req.body.Body.toUpperCase()==("CAT")){
+            catCount++;
+        }
+        twiml.message('You voted for: ' + req.body.Body + "! Visit http://3d1ba908.ngrok.io/view to see how the scores are going! 🐱"+catCount+" vs "+dogCount+"🐶");
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString());
+    }
+    else{
+        twiml.message('Dogs or Cats! Youuu deciddddeee. Reply to this message with DOG or CAT to vote.');
+  
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString());
+    }    
+  });
+
+app.get('/view', (req,res) => {
+    var obj ={
+        'catcount':catCount,
+        'dogcount':dogCount,
+    }
+
+    res.json(obj);
+})
+  
+
+app.listen(3000)
